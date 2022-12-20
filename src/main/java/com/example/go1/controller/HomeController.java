c32c6e324ab80e82c226d05ffe4395a557a0ba0c
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2022-12-20 15:12:29 +0900

    add HomeController

---
+++ src/main/java/com/example/go1/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.go1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @ResponseBody
+    public String goHome(HttpServletRequest request){
+        System.out.println("dd");
+        return "hello!";
+
+    }
+
+}
