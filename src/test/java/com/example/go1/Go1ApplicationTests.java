d61d9dff85446390c31f0d7b4177ea5fbcbeb287
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2022-12-20 14:55:14 +0900

    Initial commit

---
+++ src/test/java/com/example/go1/Go1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.go1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Go1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
