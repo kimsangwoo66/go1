7a7b2511b2004fc005ef81f5b78e6f7b73a8be95
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2022-12-22 10:20:12 +0900

    add page

---
+++ src/main/java/com/example/go1/controller/page1Controller.java
@@ -0,0 +1,21 @@
+package com.example.go1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class page1Controller {
+
+    @RequestMapping(value = "/page1", method = RequestMethod.GET)
+    @ResponseBody
+    public String goHome(HttpServletRequest request){
+        System.out.println("dd");
+        return "page1";
+
+    }
+
+}
